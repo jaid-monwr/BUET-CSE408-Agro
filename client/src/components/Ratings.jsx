946f6f16260553990c32a2982045e6b07ed58d2d
TonmoyDaFulkopi <1905085@ugrad.cse.buet.ac.bd>
2024-01-28 21:37:37 +0600

    client homepage and ecommerce side

---
+++ client/src/components/Ratings.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { CiStar } from "react-icons/ci";
+
+const Ratings = ({ ratings }) => {
+  return (
+    <>
+      {ratings >= 1 ? (
+        <span className="text-[#edbb0e]">
+          <FaStar />
+        </span>
+      ) : ratings >= 1.5 ? (
+        <span className="text-[#edbb0e]">
+          <FaStarHalfAlt />
+        </span>
+      ) : (
+        <span className="text-slate-600">
+          <CiStar />
+        </span>
+      )}
+      {ratings >= 1 ? (
+        <span className="text-[#edbb0e]">
+          <FaStar />
+        </span>
+      ) : ratings >= 0.5 ? (
+        <span className="text-[#edbb0e]">
+          <FaStarHalfAlt />
+        </span>
+      ) : (
+        <span className="text-slate-600">
+          <CiStar />
+        </span>
+      )}
+      {ratings >= 2 ? (
+        <span className="text-[#edbb0e]">
+          <FaStar />
+        </span>
+      ) : ratings >= 1.5 ? (
+        <span className="text-[#edbb0e]">
+          <FaStarHalfAlt />
+        </span>
+      ) : (
+        <span className="text-slate-600">
+          <CiStar />
+        </span>
+      )}
+      {ratings >= 3 ? (
+        <span className="text-[#edbb0e]">
+          <FaStar />
+        </span>
+      ) : ratings >= 2.5 ? (
+        <span className="text-[#edbb0e]">
+          <FaStarHalfAlt />
+        </span>
+      ) : (
+        <span className="text-slate-600">
+          <CiStar />
+        </span>
+      )}
+      {ratings >= 4 ? (
+        <span className="text-[#edbb0e]">
+          <FaStar />
+        </span>
+      ) : ratings >= 3.5 ? (
+        <span className="text-[#edbb0e]">
+          <FaStarHalfAlt />
+        </span>
+      ) : (
+        <span className="text-slate-600">
+          <CiStar />
+        </span>
+      )}
+      {ratings >= 5 ? (
+        <span className="text-[#edbb0e]">
+          <FaStar />
+        </span>
+      ) : ratings >= 4.5 ? (
+        <span className="text-[#edbb0e]">
+          <FaStarHalfAlt />
+        </span>
+      ) : (
+        <span className="text-slate-600">
+          <CiStar />
+        </span>
+      )}
+    </>
+  );
+};
+
+export default Ratings;
