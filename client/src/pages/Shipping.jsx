68fac210aa59fc37ea715dcd13791c7dba1110ea
Tonmoy Hasan <78820466+TonmoyDaFulkopi@users.noreply.github.com>
2024-02-11 20:52:00 +0600

    Update Shipping.jsx

--- client/src/pages/Shipping.jsx
+++ client/src/pages/Shipping.jsx
@@ -55,7 +55,7 @@ const Shipping = () => {
       <Header />
       <section className="bg-[url('http://localhost:3000/images/banner/1.gif')] h-[220px] mt-6 bg-cover bg-no-repeat relative bg-left">
         <div className="absolute left-0 top-0 w-full h-full bg-[#2422228a]">
-          <div className="w-[86%] md:w-[81%] sm:w-[91%] lg:w-[91%] h-full  mx-auto">
+          <div className="w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full  mx-auto">
             <div className="flex flex-col justify-center gap-1 items-center h-full w-full text-white">
               <h2 className="text-3xl font-bold">Agro</h2>
               <div className="flex justify-center items-center gap-2 text-2xl w-full">
