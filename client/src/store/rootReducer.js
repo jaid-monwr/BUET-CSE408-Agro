fcd361818b89da6191a9b1aa00baebbac4118dd9
ExcaliBUR <md.sahinakter2000@gmail.com>
2024-02-18 13:07:34 +0600

    client get produt details frontend complete

--- client/src/store/rootReducer.js
+++ client/src/store/rootReducer.js
@@ -2,12 +2,14 @@ import homeReducer from "./reducers/homeReducer";
 import authReducer from "./reducers/authReducer";
 import cartReducer from "./reducers/cartReducer";
 import orderReducer from "./reducers/orderReducer";
+import dashboardReducer from "./reducers/dashboardReducer";
 
 const rootReducers = {
   home: homeReducer,
   auth: authReducer,
   cart: cartReducer,
   order: orderReducer,
+  dashboard: dashboardReducer,
 };
 
 export default rootReducers;
